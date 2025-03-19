7f6c729a12522c287234cd3ce3ad8b0b8c36c330
Dương Unity (Jake) <jakediep1902@gmail.com>
2025-03-19 23:15:30 +0700

    #250319 Added Long

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-// Bật chuyển hướng HTTPS (có thể tắt nếu không cần)
+// Bật chuyển hướng HTTPS (có thể tắt nếu không cần)//
 //app.UseHttpsRedirection();
 if (!app.Environment.IsDevelopment())
 {
