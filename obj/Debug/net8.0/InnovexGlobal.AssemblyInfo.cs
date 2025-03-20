9bbb2b02884d3d46cb9d6d698887d33343096f3c
Dương Unity (Jake) <jakediep1902@gmail.com>
2025-03-20 23:35:19 +0700

    Added swagger

--- obj/Debug/net8.0/InnovexGlobal.AssemblyInfo.cs
+++ obj/Debug/net8.0/InnovexGlobal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InnovexGlobal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc8448bef5025a1953b3df4cf12296076e7fa072")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8eed95e7d84045b3ff07a2cd767f04a8fb97de2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("InnovexGlobal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InnovexGlobal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
